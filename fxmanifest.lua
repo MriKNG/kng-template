00fd5d01c1d19f477125e460c62db54390364192
KNG <108199777+MriKNG@users.noreply.github.com>
2023-03-04 09:06:05 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KNG'
+description 'Small resource enabling /piggyback, /carry and /takehostage for FiveM'
+version '1.0'
+
+dependencies {
+  }
+
+client_scripts {
+  }
+
+server_scripts {
+  }
